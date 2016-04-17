64e4e640e55ec95984ba1a3df49eeba32412d64f
Matt G <matt.gaikema@tamu.edu>
2016-04-16 20:53:56 -0500

    Test the function from 4.1

--- main.R
+++ main.R
@@ -6,5 +6,15 @@ library(wavelets)
 # Chapter 5 problem 1
 
 f = function(x) {
-    
+    if (x >= 0 && x < 1/4) {
+    	return(-1)
+    } else if (x >= 1/4 && x < 1/2) {
+    	return(4)
+    } else if (x >= 1/2 && x > 3/4) {
+    	return(2)	
+    } else if (x >= 3/4 && x < 1) {
+    	return(-3)
+    } else {
+    	return(NaN)
+    }
 }
