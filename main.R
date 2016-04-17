1592616ee8011e684085272a73f1117ece2e27f5
Matt G <matt.gaikema@tamu.edu>
2016-04-16 22:45:11 -0500

    confuse

--- main.R
+++ main.R
@@ -10,7 +10,7 @@ f = function(x) {
     	return(-1)
     } else if (x >= 1/4 && x < 1/2) {
     	return(4)
-    } else if (x >= 1/2 && x > 3/4) {
+    } else if (x >= 1/2 && x < 3/4) {
     	return(2)	
     } else if (x >= 3/4 && x < 1) {
     	return(-3)
@@ -18,3 +18,12 @@ f = function(x) {
     	return(NaN)
     }
 }
+
+j = 2
+k = seq(0, 1-1/2^j, by=1/(2^j))
+y = as.numeric(lapply(k,f))
+
+fdwt = dwt(X=y, filter="haar")
+print(fdwt)
+print(fdwt@W)
+print(fdwt@V)
